fa346a5ecad6ef396a1a0195923a3bae08d678c8
Jon <jon@Jons-MacBook-Pro.local>
2021-10-17 21:36:44 +0800

    updates

--- Validata.API/obj/Debug/netcoreapp3.1/Validata.API.AssemblyInfo.cs
+++ Validata.API/obj/Debug/netcoreapp3.1/Validata.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("eb0fb6d7-d2be-4a2b-b538-f9ddd5a9da5f")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Validata.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
